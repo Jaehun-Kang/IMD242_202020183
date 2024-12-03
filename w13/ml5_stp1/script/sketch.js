7efa43bd790183dc25874cff61e26cd00e9f2eaa
Jaehun-Kang <74220533+Jaehun-Kang@users.noreply.github.com>
2024-12-04 03:11:34 +0900

    20241204_0

---
+++ w13/ml5_stp1/script/sketch.js
@@ -0,0 +1,60 @@
+/*
+ * 👋 Hello! This is an ml5.js example made and shared with ❤️.
+ * Learn more about the ml5.js project: https://ml5js.org/
+ * ml5.js license and Code of Conduct: https://github.com/ml5js/ml5-next-gen/blob/main/LICENSE.md
+ *
+ * This example demonstrates face tracking on live video through ml5.faceMesh.
+ */
+
+let faceMesh;
+let video;
+let faces = [];
+let options = { maxFaces: 1, refineLandmarks: false, flipped: false };
+
+function preload() {
+  // Load the faceMesh model
+  faceMesh = ml5.faceMesh(options); //머신러닝 데이터 불러오기
+}
+
+function setup() {
+  createCanvas(640, 480);
+  // Create the webcam video and hide it
+  video = createCapture(VIDEO);
+  video.size(640, 480);
+  video.hide(); //이거 넣어야 비디오 안나옴
+  // Start detecting faces from the webcam video
+  faceMesh.detectStart(video, gotFaces); // 미디어유형, 콜백함수 넣어주기(다 찾으면 결과 콜백)
+}
+
+function draw() {
+  // Draw the webcam video
+  image(video, 0, 0, width, height);
+  // Draw all the tracked face points
+  for (let i = 0; i < faces.length; i++) {
+    let face = faces[i];
+    // for (let j = 0; j < face.keypoints.length; j++) {
+    //   let keypoint = face.keypoints[j];
+    //   fill(0, 255, 0);
+    //   noStroke();
+    //   circle(keypoint.x, keypoint.y, 5);
+    // }
+
+    let leftEye = face.leftEye;
+    for (let n = 0; n < leftEye.keypoints.length; n++) {
+      let keypoint = leftEye.keypoints[n];
+      circle(keypoint.x, keypoint.y, 5);
+    }
+  }
+}
+
+// Callback function for when faceMesh outputs data
+function gotFaces(results) {
+  // Save the output to the faces variable
+  faces = results;
+}
+
+function keyPressed() {
+  if (key == 'o' || key == 'O') {
+    console.log(faces);
+  }
+}
